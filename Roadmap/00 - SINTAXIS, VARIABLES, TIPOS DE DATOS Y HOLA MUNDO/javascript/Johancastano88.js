e8ddf559ca4ebd1332db1ce896a8f61978ca6de4
Johancastano88 <113441865+Johancastano88@users.noreply.github.com>
2024-10-12 11:11:45 -0500

    Create Johancastano88.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Johancastano88.js
@@ -0,0 +1,23 @@
+//https://www.javascript.com/
+
+/* ESTE ES OTRO COMENTARIO
+PERO LO PUEDO HACER UN POCO MAS LARGO 
+VOY A SER PROGRAMADOR */
+
+var  = "Nueva "
+var = "Otra nueva"
+
+const  = "no puedo ser otro string diferente"
+const = "soy fijo por ser una constante"
+
+var my_int = 1
+var my_float = 2.56
+var my_boolean = false
+var my_string = "el gato en el texto"
+println( "Serás exitoso") 
+
+println(type my_int)
+
+println(type(my_float))
+println(type(my_boolean))
+println(type(my_string))
